972389d347af6ab05e83c42922bbe15b33665c29
Matteo Di Gregorio <matteodgrgr@gmail.com>
2021-02-19 19:12:52 +0000

    Initial commit

---
+++ src/main/java/com/matt/springmasterclass/SpringMasterclassApplication.java
@@ -0,0 +1,13 @@
+package com.matt.springmasterclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMasterclassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMasterclassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matt/springmasterclass/SpringMasterclassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matt.springmasterclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMasterclassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
